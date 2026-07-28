39dd498aa14ed85adde173dc30de6d1e92ae66d6
unknown <hv4347172@gmail.com>
2026-07-28 15:37:13 +0530

    Fix production socket

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,10 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
-  transports: ["websocket"],
-});
+const socket = io(
+  import.meta.env.VITE_API_URL.replace("/api", ""),
+  {
+    transports: ["websocket"],
+  }
+);
 
 export default socket;
